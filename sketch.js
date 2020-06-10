13f502e188a2e6612898aaf39eedd203beefaa5f
Unlisted-girl <seerat.kaur@gmail.com>
2020-06-10 19:53:24 +0530

    Created Platform

--- sketch.js
+++ sketch.js
@@ -4,8 +4,8 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
-
+var platform;
+var backgroundImg;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
